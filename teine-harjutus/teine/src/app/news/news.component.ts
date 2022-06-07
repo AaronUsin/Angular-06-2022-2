13658a4172ad7a413453d801ce1b824cfb2007ea
AaronUsin <106862950+AaronUsin@users.noreply.github.com>
2022-06-07 20:05:32 +0300

    HTML menüü jätkamine (news)

--- teine-harjutus/teine/src/app/news/news.component.ts
+++ teine-harjutus/teine/src/app/news/news.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news.component.css']
 })
 export class NewsComponent implements OnInit {
-
+uudis = "Juhtus selline asi ...";
+uudiseSisu = "See on uudise sisu";
+onInglisekeelne = false;
+onSoomekeelne = false;
+ollakseHiiregaT2rniPeal = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  hiiregaYleMinnes(){
+    this.ollakseHiiregaT2rniPeal = true;
+  }
+
+  hiirega2raMinnes(){
+    this.ollakseHiiregaT2rniPeal = false;
+  }
+
+  muudaIngliseKeelseks(){
+    this.uudis = "This thing happened...";
+    this.uudiseSisu = "This is the content of the news";
+    this.onInglisekeelne = true;
+    this.onSoomekeelne = false;
+  }
+  muudaSoomeKeelseks(){
+    this.uudis = "Tämä tapahtui ..."
+    this.uudiseSisu = "Tämä on uutisen sisältö"
+    this.onInglisekeelne = false;
+    this.onSoomekeelne = true;
+  }
+  muudaEestiKeelseks(){
+    this.uudis = "Juhtus selline asi ...";
+    this.uudiseSisu = "See on uudise sisu";
+    this.onInglisekeelne = false;
+    this.onSoomekeelne = false;
+  }
 }
