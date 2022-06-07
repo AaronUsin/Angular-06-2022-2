ec88058d51138f2c7a0974cfbf24c64b2b50c2a6
AaronUsin <106862950+AaronUsin@users.noreply.github.com>
2022-06-07 22:26:47 +0300

    Html menüü contact täiendus

--- teine-harjutus/teine/src/app/contact/contact.component.ts
+++ teine-harjutus/teine/src/app/contact/contact.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
+aadress = "Aadress: Nurme 32 Tallinn 10322";
+telefoniNumber = "Telefon: 57439312";
+email = "Email: aaron.usin@gmail.com"
+onInglisekeelne = false;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+muudaIngliseKeelseks() {
+  this.aadress = "Address: Nurme 32 Tallinn 10322"
+  this.telefoniNumber = "Phone nr: 57439312";
+  this.email = "Email: aaron.usin@gmail.com"
+  this.onInglisekeelne = true;
+}
 }
