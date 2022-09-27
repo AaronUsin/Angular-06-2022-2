3f4e688ea96143b67d077adb7415d7eb8f821beb
AaronUsin <106862950+AaronUsin@users.noreply.github.com>
2022-09-27 23:27:24 +0300

    Navbar to the middle and Footer

--- NBK/src/app/app.module.ts
+++ NBK/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
